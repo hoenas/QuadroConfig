a14743bee2b66f81e2ed4f675cb44ff2a4d898fc
hoenas <hoenas@googlemail.com>
2015-04-30 10:30:40 +0200

    initial commi

--- QuadroConfig/src/Main.java
+++ QuadroConfig/src/Main.java
@@ -1,9 +1,216 @@
-import java.io.*;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import java.awt.Component;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+
+import java.awt.RenderingHints.Key;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import org.jfree.*;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.Plot;
+import org.jfree.data.general.Dataset;
+import org.jfree.data.general.DefaultPieDataset;
+
+import javax.swing.JProgressBar;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ImageObserver;
+import java.awt.image.RenderedImage;
+import java.awt.image.renderable.RenderableImage;
+
+import javax.swing.SwingConstants;
+
+import java.awt.Color;
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+
+import java.text.AttributedCharacterIterator;
 import java.util.*;
+import java.awt.Composite;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
+import java.awt.Canvas;
+import java.awt.Image;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.Stroke;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.SpringLayout;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+
+import net.miginfocom.swing.MigLayout;
+
+import java.awt.CardLayout;
+import org.jfree.ui.*;
 
 public class Main {
 
+	private JFrame frame;
+	private static boolean messungAktiv = false;
+	
+	private static Timer messtimer;
+	private static SerialPortEventListener portListener;
+	private static int messintervall = 250;
+	
+	private static SerialPort port;
+	
+	// Befehle für Protokoll
+	private static byte COMMAND_MEASUREMENT = (byte)0b10000000;
+	private static byte COMMAND_RESET = (byte)0b01000000;
+	private static byte COMMAND_CONFIGURATION = (byte)0b00100000;
+	
+	private static byte[] buffer;
+	private static int bufferLength = 50;
+	
+	// Visuelle Darstellung
+	private static Plot plots[];
+	private static int plotHistoryLength;
+	
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+					
+					// Port-Konfiguration aufrufen
+					PortConfig config = new PortConfig();
+					config.setVisible(true);
+					
+					// Port konfigurieren
+					port = new SerialPort( config.portName );
+					port.setParams(config.baudRate, config.databits, config.stoppbits, config.parity);
+					port.openPort();
+					portListener = new SerialPortEventListener() {
+						
+						@Override
+						public void serialEvent(SerialPortEvent arg0) {
+							// TODO: Protokoll implementieren
+						}
+					};
+					port.addEventListener( portListener );
+					
+					buffer = new byte[ bufferLength ];
+					plots = new Plot[ plotHistoryLength ];
+					
+					Rectangle rect = new Rectangle(0, 0, 500, 500);
+					
+					JFreeChart mychart = new JFreeChart(plots[0]);
+					mychart.setAntiAlias(true);
+					
+
+					
+					// mychart.draw(gfx, rect);				
+					
+					// Timerkonfiguration
+					messtimer = new Timer();
+					messtimer.scheduleAtFixedRate(new TimerTask() {
+						public void run() {
+							if(messungAktiv) {
+								try {
+									// Daten anfordern
+									System.out.println("LOLLINGER");
+									//port.writeByte(COMMAND_MEASUREMENT);
+								} catch( Exception e ) {
+									System.out.println( e.getMessage() );
+								}
+							}
+						}
+					}, 0, messintervall);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 763, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		JButton btnMessungStartenpausieren = new JButton("Messung starten");
+		menuBar.add(btnMessungStartenpausieren);
+		btnMessungStartenpausieren.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (!messungAktiv) {
+					btnMessungStartenpausieren.setText("Messung pausieren");
+				} else {
+					btnMessungStartenpausieren.setText("Messung starten");
+				}
+				messungAktiv = !messungAktiv;
+			}
+		});
+		btnMessungStartenpausieren.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+
+			}
+		});
+
+		JButton btnQuadrokopterKonfigurieren = new JButton(
+				"Quadrokopter konfigurieren");
+		btnQuadrokopterKonfigurieren.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+		});
+		menuBar.add(btnQuadrokopterKonfigurieren);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
 		
+		Canvas canvas = new Canvas();
+		frame.getContentPane().add(canvas, "name_10652026702458");
+
+	}
+
+	private void updateGraphs() {
+		System.out.println("Update");
 	}
 }
---
+++ QuadroConfig/src/PortConfig.java
@@ -0,0 +1,228 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+
+import jssc.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PortConfig extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtCom;
+
+	public String portName;
+	public int baudRate;
+	public int databits;
+	public int stoppbits;
+	public int parity;
+	public boolean ok;
+
+	/**
+	 * Launch the application.
+	 */
+
+	public static void main(String[] args) {
+		try {
+			PortConfig dialog = new PortConfig();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public PortConfig() {
+		setBounds(100, 100, 213, 214);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[] { 0, 0, 0 };
+		gbl_contentPanel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		gbl_contentPanel.columnWeights = new double[] { 0.0, 1.0,
+				Double.MIN_VALUE };
+		gbl_contentPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, Double.MIN_VALUE };
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblPortname = new JLabel("Port-Name:");
+			lblPortname.setHorizontalAlignment(SwingConstants.LEFT);
+			GridBagConstraints gbc_lblPortname = new GridBagConstraints();
+			gbc_lblPortname.anchor = GridBagConstraints.WEST;
+			gbc_lblPortname.insets = new Insets(0, 0, 5, 5);
+			gbc_lblPortname.gridx = 0;
+			gbc_lblPortname.gridy = 0;
+			contentPanel.add(lblPortname, gbc_lblPortname);
+		}
+		{
+			txtCom = new JTextField();
+			txtCom.setText("COM4");
+			GridBagConstraints gbc_txtCom = new GridBagConstraints();
+			gbc_txtCom.insets = new Insets(0, 0, 5, 0);
+			gbc_txtCom.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtCom.gridx = 1;
+			gbc_txtCom.gridy = 0;
+			contentPanel.add(txtCom, gbc_txtCom);
+			txtCom.setColumns(10);
+		}
+		{
+			JLabel lblBaudrate = new JLabel("Baud-Rate:");
+			lblBaudrate.setHorizontalAlignment(SwingConstants.LEFT);
+			GridBagConstraints gbc_lblBaudrate = new GridBagConstraints();
+			gbc_lblBaudrate.anchor = GridBagConstraints.WEST;
+			gbc_lblBaudrate.insets = new Insets(0, 0, 5, 5);
+			gbc_lblBaudrate.gridx = 0;
+			gbc_lblBaudrate.gridy = 1;
+			contentPanel.add(lblBaudrate, gbc_lblBaudrate);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			comboBox.addItem(SerialPort.BAUDRATE_110);
+			comboBox.addItem(SerialPort.BAUDRATE_300);
+			comboBox.addItem(SerialPort.BAUDRATE_600);
+			comboBox.addItem(SerialPort.BAUDRATE_1200);
+			comboBox.addItem(SerialPort.BAUDRATE_4800);
+			comboBox.addItem(SerialPort.BAUDRATE_9600);
+			comboBox.addItem(SerialPort.BAUDRATE_14400);
+			comboBox.addItem(SerialPort.BAUDRATE_38400);
+			comboBox.addItem(SerialPort.BAUDRATE_57600);
+			comboBox.addItem(SerialPort.BAUDRATE_115200);
+			comboBox.addItem(SerialPort.BAUDRATE_128000);
+			comboBox.setSelectedIndex(9);
+			baudRate = SerialPort.BAUDRATE_115200;
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 1;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+			JLabel label = new JLabel("");
+			GridBagConstraints gbc_label = new GridBagConstraints();
+			gbc_label.insets = new Insets(0, 0, 5, 5);
+			gbc_label.gridx = 0;
+			gbc_label.gridy = 2;
+			contentPanel.add(label, gbc_label);
+		}
+		{
+			JLabel lblDatenbits = new JLabel("Datenbits:");
+			lblDatenbits.setHorizontalAlignment(SwingConstants.LEFT);
+			GridBagConstraints gbc_lblDatenbits = new GridBagConstraints();
+			gbc_lblDatenbits.anchor = GridBagConstraints.WEST;
+			gbc_lblDatenbits.insets = new Insets(0, 0, 5, 5);
+			gbc_lblDatenbits.gridx = 0;
+			gbc_lblDatenbits.gridy = 3;
+			contentPanel.add(lblDatenbits, gbc_lblDatenbits);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			comboBox.addItem(SerialPort.DATABITS_5);
+			comboBox.addItem(SerialPort.DATABITS_6);
+			comboBox.addItem(SerialPort.DATABITS_7);
+			comboBox.addItem(SerialPort.DATABITS_8);
+			comboBox.setSelectedIndex(3);
+			databits = SerialPort.DATABITS_8;
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 3;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+			JLabel lblStoppbits = new JLabel("Stoppbits:");
+			lblStoppbits.setHorizontalAlignment(SwingConstants.LEFT);
+			GridBagConstraints gbc_lblStoppbits = new GridBagConstraints();
+			gbc_lblStoppbits.anchor = GridBagConstraints.WEST;
+			gbc_lblStoppbits.insets = new Insets(0, 0, 5, 5);
+			gbc_lblStoppbits.gridx = 0;
+			gbc_lblStoppbits.gridy = 4;
+			contentPanel.add(lblStoppbits, gbc_lblStoppbits);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			comboBox.addItem(SerialPort.STOPBITS_1);
+			comboBox.addItem(SerialPort.STOPBITS_2);
+			comboBox.addItem(SerialPort.STOPBITS_1_5);
+			comboBox.setSelectedIndex(0);
+			stoppbits = SerialPort.STOPBITS_1;
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 4;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+			JLabel lblPartitt = new JLabel("Partit\u00E4t:");
+			lblPartitt.setHorizontalAlignment(SwingConstants.LEFT);
+			GridBagConstraints gbc_lblPartitt = new GridBagConstraints();
+			gbc_lblPartitt.anchor = GridBagConstraints.WEST;
+			gbc_lblPartitt.insets = new Insets(0, 0, 0, 5);
+			gbc_lblPartitt.gridx = 0;
+			gbc_lblPartitt.gridy = 5;
+			contentPanel.add(lblPartitt, gbc_lblPartitt);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			comboBox.addItem("keine");
+			comboBox.addItem("gerade");
+			comboBox.addItem("ungerade");
+			comboBox.setSelectedIndex(2);
+			parity = SerialPort.PARITY_EVEN;
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 5;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+
+		}
+
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
